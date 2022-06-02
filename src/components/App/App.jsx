fecc8c1982245d33ef8f8dcb86541b767312dc0d
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-06-02 14:51:19 +0600

    feat: Настроить начальный роутинг

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,21 @@
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+
+//? Импорты компонентов
+import Main from '../Main/Main.jsx';
+import Register from '../Register/Register.jsx'
+import Login from '../Login/Login.jsx'
 
 function App() {
-
+  return (
+    <div className="page">
+      <Switch>
+        <Route exact path="/"><Main /></Route>
+        <Route path="/sign-in"><Login /></Route>
+        <Route path="/sign-up"><Register /></Route>
+      </Switch >
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App.jsx';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+//? Импорты компонентов
+import App from './components/App/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
