50b77e62397f32f2e3b7e3d02c9fbc41352e1c5d
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-06-03 14:10:22 +0600

    refactor: Заменить button на Link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,15 +10,15 @@ export default function Header({ isLogin }) {
       <Link to='/' className="header__logo"></Link>
       <Navigation isLogin={isLogin} />
       <div className="header__auth-buttons">
-        <button type="button" className={`header__button register-button  ${isLogin ? 'hide-block' : ''}`}>
-          <Link className="header__button-link" to='sign-up'>Регистрация</Link>
-        </button>
-        <button type="button" className={`header__button login-button  ${isLogin ? 'hide-block' : ''}`}>
-          <Link className="header__button-link" to='sign-up'>Войти</Link>
-        </button>
-        <button type="button" className={`header__button profile-button ${isLogin ? '' : 'hide-block'}`}>
-          <Link className="header__button-link" to='sign-up'>Аккаунт</Link>
-        </button>
+        <Link to='sign-up' className={`header__link register-link ${isLogin ? 'hide-block' : ''}`}>
+          Регистрация
+        </Link>
+        <Link to='sign-in' className={`header__link login-link ${isLogin ? 'hide-block' : ''}`}>
+          Войти
+        </Link>
+        <Link to='profile' className={`header__link profile-link ${isLogin ? '' : 'hide-block'}`}>
+          Аккаунт
+        </Link>
       </div>
     </header >
   );
