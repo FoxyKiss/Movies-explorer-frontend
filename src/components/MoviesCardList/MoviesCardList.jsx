182e6b3ceac387616bc53b9610506abe5b76ed49
Arsentiy Kravtsov <foxkiss98@gmail.com>
2022-06-05 12:15:35 +0600

    refactor: Перенести массив фильмов

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,79 +1,11 @@
 import './MoviesCardList.css';
-import image from '../../images/film.png';
-
 import MoviesCard from '../MoviesCard/MoviesCard.jsx'
 
-export default function MoviesCardList() {
-  const moviesList =
-    [
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '5',
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '4',
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '3',
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '1',
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '6'
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '10'
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '0'
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '1234'
-      },
-
-      {
-        name: '33 Слова о дизайне',
-        image: image,
-        duration: '1ч 47м',
-        movieId: '90'
-      },
-
-    ];
-
+export default function MoviesCardList({ moviesList }) {
   function like(evt) {
     evt.target.classList.toggle('movies-info__like-button_active')
   }
+
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__list">
